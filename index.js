559d8929db1b532b20e63dcb2a10942a77f54c3a
Prabhat Saini <Prabhat.saini@binmile.com>
2024-08-07 16:17:58 +0530

    cors Frontend Link in cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(
     cors({
-        origin: '*',
+        origin: 'https://movie-frontend-dep.onrender.com/',
         credential: true,
     })
 );
