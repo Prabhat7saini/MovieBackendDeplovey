dea3dc76794137eb095384f84c3707cbf46cb54e
Prabhat Saini <Prabhat.saini@binmile.com>
2024-08-07 16:28:08 +0530

    *

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(
     cors({
-        origin: 'https://movie-frontend-dep.onrender.com',
+        origin: '*',
         credential: true,
     })
 );
